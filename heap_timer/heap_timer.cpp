c1febb08472aecfb9ccf8de2eff3ed800c3c097f
doraemon-hub-art <1660219734@qq.com>
2022-11-20 21:58:38 +0800

    heap_timer

---
+++ heap_timer/heap_timer.cpp
@@ -0,0 +1,6 @@
+// 
+// Created by xuanxuan on 2022/11/20.
+//
+
+#include "heap_timer.h"
+
---
+++ heap_timer/heap_timer.h
@@ -0,0 +1,28 @@
+// 
+// Created by xuanxuan on 2022/11/20.
+//
+
+#ifndef TINYWEBSERVER_HEAP_TIMER_H
+#define TINYWEBSERVER_HEAP_TIMER_H
+
+
+#include <queue>
+#include <unordered_map>
+#include <time.h>
+#include <algorithm>
+#include <arpa/inet.h>
+#include <functional>
+#include <assert.h>
+#include <chrono>
+#include "../log/log.h"
+
+typedef  std::function<void()> time_out_call_back;
+
+
+// 最小堆-定时器
+class heap_timer {
+
+};
+
+
+#endif //TINYWEBSERVER_HEAP_TIMER_H
