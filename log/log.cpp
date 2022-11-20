22e470190067bdc38778d8abc2e87030bec07935
doraemon-hub-art <1660219734@qq.com>
2022-11-20 11:52:08 +0800

    TinyWebServer

---
+++ log/log.cpp
@@ -0,0 +1,5 @@
+// 
+// Created by xuanxuan on 2022/11/20.
+//
+
+#include "log.h"
---
+++ log/log.h
@@ -0,0 +1,14 @@
+// 
+// Created by xuanxuan on 2022/11/20.
+//
+
+#ifndef TINYWEBSERVER_LOG_H
+#define TINYWEBSERVER_LOG_H
+
+
+class log {
+
+};
+
+
+#endif //TINYWEBSERVER_LOG_H
