ec697723d451db92efec775520a75f43f24c4842
doraemon-hub-art <1660219734@qq.com>
2022-11-21 23:47:00 +0800

    heap_timer

--- heap_timer/heap_timer.h
+++ heap_timer/heap_timer.h
@@ -7,21 +7,94 @@
 
 
 #include <queue>
+#include <vector>
 #include <unordered_map>
 #include <time.h>
 #include <algorithm>
 #include <arpa/inet.h>
 #include <functional>
 #include <assert.h>
-#include <chrono>
+#include <chrono> // 时钟类
 #include "../log/log.h"
 
-typedef  std::function<void()> time_out_call_back;
+// fd为指定的client socket fd，在m_ref中保存着其在堆数组中的下标。
 
+// 回调函数
+typedef std::function<void()> TIME_OUT_CALl_BACK;
+
+// 高精度时钟 now()方法获取当前时间
+typedef std::chrono::high_resolution_clock CLOCK;
+
+// 毫秒
+typedef std::chrono::milliseconds MS;
+
+// 指向时间中的一个点
+typedef CLOCK::time_point TIME_STAMP;
+
+// 堆中结点-定时器
+struct timer_node{
+    int fd; // socket描述符
+    TIME_STAMP expires; // 定时器生效的绝对时间
+    TIME_OUT_CALl_BACK cb;// 回调函数
+    bool operator<(const timer_node& t){
+        return expires < t.expires;
+    }
+};
+
+// 最小堆是指每个结点的值都小于或等于其子结点的值的完全二叉树
 
 // 最小堆-定时器
 class heap_timer {
 
+public:
+
+    heap_timer();
+
+    ~heap_timer();
+
+    //  调整指定
+    void adjust(int fd,int new_expires);
+
+    // 添加结点-client socket fd-超时时间-回调函数
+    void add(int fd,int timeout,const TIME_OUT_CALl_BACK & cb);
+
+    // 执行对应的回调函数——执行任务-超时了调用。
+    void do_work(int fd);
+
+    // 清空时间堆
+    void clear();
+
+    // 检查堆中是否有超时的定时器
+    void tick();
+
+    //  删除堆顶元素
+    void pop();
+
+
+    int get_next_tick();
+
+private:
+
+    // 删除索引为index的定时器
+    void del(size_t index);
+
+    // 向上调整
+    void sift_up(size_t i);
+
+    // 向下调整-确保每个结点有最小堆的性质，要调整的定时器索引，堆数组结尾下标。
+    bool sift_down(size_t index,size_t n);
+
+    // 交换堆内两个结点的位置，用于调整对时间堆的调整。
+    void swap_node(size_t i,size_t j);
+
+
+private:
+
+    // 堆用数组实现
+    std::vector<timer_node> m_heap;
+
+    // 哈希表中的映射——<客户端socket fd,该fd在m_heap中的下标>
+    std::unordered_map<int,size_t> m_ref;
 };
 
 
