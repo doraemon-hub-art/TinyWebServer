959f00e692b8121c2830cb2e1b0b303d18534ed3
doraemon-hub-art <1660219734@qq.com>
2022-11-22 23:36:36 +0800

    sql_conn_pool

---
+++ sql_conn_pool/sql_conn_pool.cpp
@@ -0,0 +1,5 @@
+// 
+// Created by xuanxuan on 2022/11/22.
+//
+
+#include "sql_conn_pool.h"
---
+++ sql_conn_pool/sql_conn_pool.h
@@ -0,0 +1,22 @@
+// 
+// Created by xuanxuan on 2022/11/22.
+//
+
+#ifndef TINYWEBSERVER_SQL_CONN_POOL_H
+#define TINYWEBSERVER_SQL_CONN_POOL_H
+
+#include <mysql/mysql.h>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
+#include <thread>
+#include "../log/log.h"
+
+
+class sql_conn_pool {
+
+};
+
+
+#endif //TINYWEBSERVER_SQL_CONN_POOL_H
