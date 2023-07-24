d20e3fe96aa92d87bb1b9dc6f0b8bfeb89e37ea8
doraemon-hub-art <1660219734@qq.com>
2023-07-24 14:29:33 +0800

    test

---
+++ build/CMakeFiles/TinyWebServer.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xuanxuan/coding/ws/TinyWebServer/buffer/buffer.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/buffer/buffer.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/epoller/epoller.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/epoller/epoller.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/heap_timer/heap_timer.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/heap_timer/heap_timer.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/http/http_conn/http_conn.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/http/http_conn/http_conn.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/http/http_request/http_request.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/http/http_request/http_request.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/http/http_response/http_response.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/http/http_response/http_response.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/log/log.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/log/log.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/main.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/main.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/server/server.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/server/server.cpp.o"
+  "/home/xuanxuan/coding/ws/TinyWebServer/sql_conn_pool/sql_conn_pool.cpp" "/home/xuanxuan/coding/ws/TinyWebServer/build/CMakeFiles/TinyWebServer.dir/sql_conn_pool/sql_conn_pool.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/mysql"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TinyWebServer.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TinyWebServer.dir/buffer/buffer.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/epoller/epoller.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/heap_timer/heap_timer.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/http/http_conn/http_conn.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/http/http_request/http_request.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/http/http_response/http_response.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/log/log.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/main.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/server/server.cpp.o"
+  "CMakeFiles/TinyWebServer.dir/sql_conn_pool/sql_conn_pool.cpp.o"
+  "TinyWebServer"
+  "TinyWebServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TinyWebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
